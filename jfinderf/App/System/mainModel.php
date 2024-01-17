83706cd5956cdfd74b6e2968ea34697ad9117e48
yusufnrn <108307323+yusufnrn@users.noreply.github.com>
2024-01-17 19:38:20 +0300

    Delete jfinderf directory

--- jfinderf/App/System/mainModel.php
+++
@@ -1,8 +0,0 @@
-<?php
-class mainModel extends Api{
-    protected static $Api;
-    public function __construct()
-    {
-        self::$Api = new Api();
-    }
-}
--- jfinderf/App/route.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-App::getAction('/','/', false);
-App::getAction('','', false);
-App::getAction('/index','/index/index', false, 'frontend');
-App::getAction('/index/([0-9a-zA-Z_]+)','/index/index/([0-9a-zA-Z_]+)', false, 'frontend');
-
